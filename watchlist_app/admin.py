45bac92a626220495fc43bbf2b7387e73770527b
Disha Arora <dishaarora1996@gmail.com>
2023-05-25 15:19:07 +0530

    imdb clone complete project

---
+++ watchlist_app/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+from .models import *
+
+# Register your models here.
+
+admin.site.register(WatchList)
+admin.site.register(StreamPlatform)
+admin.site.register(Review)
